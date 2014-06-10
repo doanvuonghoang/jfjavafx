bcb438ab906602537f125efc6bad28b90044111b
Hoàng <Hong@192.168.1.3>
2014-06-11 01:52:39 +0700

    create core functions

--- jfjavafx/src/com/jf/javafx/Application.java
+++ jfjavafx/src/com/jf/javafx/Application.java
@@ -17,20 +17,71 @@
 
 package com.jf.javafx;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Properties;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.event.EventHandler;
+import javafx.event.EventType;
 import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
 
 /**
  *
  * @author Hoàng
  */
 public class Application extends javafx.application.Application {
+    protected static String JF_HOME;
+    protected static String JF_CONF;
+    private static Properties properties;
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        initEnvVars();
+        initStage(primaryStage);
     }
     
     public static void main(String[] args) {
         launch(args);
+    }
+
+    private void initEnvVars() throws Exception {
+        JF_HOME = (new File(Application.class.getProtectionDomain().getCodeSource().getLocation().getPath())).getParent();
+        
+        JF_CONF = JF_HOME + File.separator + "conf";
+        File f = new File(JF_CONF);
+        if(!f.exists()) f.mkdirs();
+        
+        properties = new Properties();
+        f = new File(getJFPropertiesFile());
+        if(!f.exists()) f.createNewFile();
+        else properties.load(new FileInputStream(f));
+    }
+
+    private void initStage(Stage primaryStage) {
+        primaryStage.setTitle(JF_HOME);
+        
+        primaryStage.onShownProperty().addListener((ObservableValue<? extends EventHandler<WindowEvent>> observable, EventHandler<WindowEvent> oldValue, EventHandler<WindowEvent> newValue) -> {
+            commitProperties();
+        });
+        
+        primaryStage.show();
+    }
+
+    protected String getJFPropertiesFile() {
+        return JF_CONF + File.separator + "jf.properties";
+    }
+
+    private void commitProperties() {
+        try {
+            properties.store(new FileOutputStream(getJFPropertiesFile()), "");
+        } catch (IOException ex) {
+            Logger.getLogger(Application.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 }
